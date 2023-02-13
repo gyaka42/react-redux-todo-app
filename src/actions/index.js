951acaa5f342f9d0b4e5f1ac2740bc0bd886f235
gyaka42 <g_yaka@hotmail.com>
2023-02-13 21:10:14 +0100

    added screen gif and setup the README.md

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,14 +3,13 @@ export const TEMIZLE = "TEMIZLE";
 export const ISARETLE = "ISARETLE";
 
 export const listeyeEkle = (text) => {
-    return { type: EKLE, payload: text }
-}
+  return { type: EKLE, payload: text };
+};
 
+export const isaretle = (id) => {
+  return { type: ISARETLE, payload: id };
+};
 
-export const isaretle = id => {
-    return { type: ISARETLE, payload: id};
-}
-
-export const temizle =  () => {
-    return{ type: TEMIZLE}
-}
+export const temizle = () => {
+  return { type: TEMIZLE };
+};
